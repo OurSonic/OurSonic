10c6f2b739a45c203ca37dd598245707a8c5ff9f
Salvatore <dested@gmail.com>
2012-01-26 05:06:00 -0700

    whoops

--- javascript/SonicEngine.js
+++ javascript/SonicEngine.js
@@ -432,3 +432,36 @@ function SonicEngine(canvasName) {
 
 
 
+function Sonic() {
+    this.x = 0;
+    this.y = 0;
+    this.sprite = new Image();
+    var sprite1 = this.sprite;
+    this.sprite.onload = function () {
+        sprite1.loaded = true;
+    };
+    var j = "assets/Sprites/sonic.png";
+    this.sprite.src = j;
+
+
+
+    this.draw = function (canvas, scale) {
+        if(this.sprite.loaded)
+           canvas.drawImage(this.sprite, this.x, this.y);
+    };
+    this.tick = function() {
+
+    };
+    this.pressJump = function () {
+        this.y -= 5;
+    };
+    this.pressCrouch= function () {
+        this.y += 5;
+    };
+    this.pressLeft= function () {
+        this.x -= 5;
+    };
+    this.pressRight= function () {
+        this.x += 5;
+    };
+}
